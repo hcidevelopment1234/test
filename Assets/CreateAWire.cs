6fd8d6483f87b500737a82f9b5fd26f962964474
Sharma <hitesh.sharma.ext@siemens.com>
2016-10-11 12:25:25 -0400

    test

---
+++ Assets/CreateAWire.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateAWire : MonoBehaviour {
+    public GameObject wire;
+    public GameObject wire2;
+    int i = 0;
+    int j = 0;
+    public float smooth = 2.0F;
+    public float tiltAngle = 30.0F;
+    // Use this for initialization
+    void Start () {
+	
+	}
+    public void CreateWire()
+    {
+        Debug.Log("clicked");
+        //for (int i = 0; i < 5; i++)
+        //{
+            Instantiate(wire, new Vector3(i * 5.0f, 0, 100), Quaternion.identity);
+            i++;
+            //Instantiate(enemy);
+        //}
+    }
+
+   
+
+    public void CreateWire2()
+    {
+        Debug.Log("clicked");
+        //for (int i = 0; i < 5; i++)
+        //{
+       
+        Instantiate(wire2, new Vector3(i * 8.0f, 0, 100), transform.rotation);
+        i++;
+        //Instantiate(enemy);
+        //}
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+    }
+}
---
+++ Assets/Drag.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drag : MonoBehaviour {
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void Start()
+    {
+        Debug.Log("drag script");
+
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse down");
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Debug.Log("Dragging");
+        Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+        transform.position = cursorPosition;
+    }
+
+    
+}
